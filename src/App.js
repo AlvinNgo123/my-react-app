282f9f0f5d489ae71259f2b9c1478e72cceb11d6
Alvin Ngo <aln011@ucsd.edu>
2022-02-14 00:13:41 -0800

    Tyring to push first react app to github

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const sayHello = () => {
+    console.log("sup");
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, this is Alvin.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={sayHello}>Peace</button>
       </header>
     </div>
   );
