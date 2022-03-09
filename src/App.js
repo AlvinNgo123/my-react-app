78794de5eb3f247f3c45d9ca9771040b458d1a3f
Alvin Ngo <aln011@ucsd.edu>
2022-03-09 13:54:17 -0800

    Added Firebase functionality to code. Only hardcode push right now

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
-import { BrowserRouter as Router, Switch, Route, Redirect,} from "react-router-dom";
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase, ref, set } from "firebase/database";
+
 import NewTicket from "./NewTicket.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB6W1H-nXmSRnvmKITBKBxwYmqxtri-Pjc",
+  authDomain: "ticketing-ba7e8.firebaseapp.com",
+  projectId: "ticketing-ba7e8",
+  storageBucket: "ticketing-ba7e8.appspot.com",
+  messagingSenderId: "1077069548792",
+  appId: "1:1077069548792:web:dc2dfbe6f1fd16519d7f0d",
+  measurementId: "G-8BS6G0NFNZ"
+};
+
+const application = initializeApp(firebaseConfig);
+const analytics = getAnalytics(application);
+const db = getDatabase();
 
 function App() {
 
+  //ticket -> ticketnumber -> [user, email, message, status]
   const createNewTicket = () => {
     console.log("Create New Ticket button was clicked.");
-    return(
-      <NewTicket/>
-    );
+
+    let ticketID = Math.random().toString(36).slice(2);
+
+    set(ref(db, 'tickets/' + ticketID), {
+      username: "Alvin",
+      email: "alvin email",
+      subject: "my subj",
+      description: "My ticket description",
+      status: "Open"
+    });
+
+
   }
 
   return (
@@ -20,26 +48,10 @@ function App() {
         <p>
           Hello, this is Alvin.
         </p>
-        <Router>
-        <Switch>
-          {/* This route is for home component 
-          with exact path "/", in component props 
-          we passes the imported component*/}
-          <Route exact path="/" component={App} />
-
-          {/* This route is for about component 
-          with exact path "/newticket", in component 
-          props we passes the imported component*/}
-          <Route path="/newticket" component={NewTicket} />
-            
-          {/* If any route mismatches the upper 
-          route endpoints then, redirect triggers 
-          and redirects app to home component with to="/" */}
-          <Redirect to="/" />
-        </Switch>
-      </Router>
-
-        <Button onClick={createNewTicket} onvariant="contained" size="large" startIcon={<AddCircleIcon />} style={{backgroundColor: "#4CAF50", color: "white"}}>Create a New Ticket</Button>
+        
+        <div><NewTicket/></div>
+        
+        <Button onClick={createNewTicket} onvariant="contained" size="large" startIcon={<AddCircleIcon />} style={{backgroundColor: "#4CAF50", color: "white"}}>Create a new ticket</Button>
         
       </header>
       
