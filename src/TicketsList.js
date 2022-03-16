bb8d83e3385783f08399212c1ca45f1e7ed2761f
Sintahks <10714217+Sintahks@users.noreply.github.com>
2022-03-16 10:30:36 -0700

    Fixed realtime bug issue and data fetching issue

--- src/TicketsList.js
+++ src/TicketsList.js
@@ -10,6 +10,7 @@ import Paper from '@mui/material/Paper';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getDatabase, ref, onValue } from "firebase/database";
+import { useEffect } from 'react';
 
 const firebaseConfig = {
     apiKey: "AIzaSyB6W1H-nXmSRnvmKITBKBxwYmqxtri-Pjc",
@@ -28,19 +29,28 @@ const firebaseConfig = {
 function TicketsList() {
 
     let ticketArrRef = ref(db, 'tickets/');
+    const [rows, setrows] = React.useState([]); 
 
     function createData(username, email, subject, description, status) {
         return { username, email, subject, description, status };
     }
 
-    //table rows to be filled by Firebase DB!
-    const rows = [];
-    onValue(ticketArrRef, (snapshot) => {
-        const data = snapshot.val();
-        for(var id in data){
-            rows.push(createData(data[id]['username'], data[id]['email'], data[id]['subject'], data[id]['description'], data[id]['status']));
-        }
-    })
+    useEffect( () => {
+        //table rows to be filled by Firebase DB!
+         onValue(ticketArrRef, (snapshot) => {
+            let temp_rows = [];
+            console.log("Retrieving data");
+            const data = snapshot.val();
+            console.log(data);
+            for(var id in data){
+                temp_rows.push(createData(data[id]['username'], data[id]['email'], data[id]['subject'], data[id]['description'], data[id]['status']));
+            }
+
+            if( temp_rows.length !== rows.length ){
+                setrows(temp_rows);
+            }
+        });
+    });
 
     return (
         <div className="TicketsList">
@@ -49,7 +59,7 @@ function TicketsList() {
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
                 <TableRow>
-                    <TableCell>Name</TableCell>
+                    <TableCell align="right">Name</TableCell>
                     <TableCell align="right">Email</TableCell>
                     <TableCell align="right">Subject Line</TableCell>
                     <TableCell align="right">Description</TableCell>
@@ -62,7 +72,7 @@ function TicketsList() {
                     key={row.username}
                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                     >
-                    <TableCell component="th" scope="row">
+                    <TableCell align="right" component="th" scope="row">
                         {row.username}
                     </TableCell>
                     <TableCell align="right">{row.email}</TableCell>
@@ -76,7 +86,7 @@ function TicketsList() {
             </TableContainer>
         </div>
     );
-
+ 
 
 }
 
